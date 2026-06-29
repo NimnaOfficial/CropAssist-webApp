ace181649d724ec1ffcac7a0731dd9eb76f9392c
NimnaOfficial <codse251f-003@student.nibm.lk>
2026-06-29 14:36:38 +0530

    add project

---
+++ cropAssist/src/test/java/org/ead2/cropassist/CropAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ead2.cropassist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropAssistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
