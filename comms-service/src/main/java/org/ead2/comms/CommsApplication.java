39271129be8a864088c9a9b36c666fd76ccff031
NimnaOfficial <codse251f-003@student.nibm.lk>
2026-06-30 14:53:35 +0530

    fix spring runnable files

---
+++ comms-service/src/main/java/org/ead2/comms/CommsApplication.java
@@ -0,0 +1,11 @@
+package org.ead2.comms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CommsApplication.class, args);
+    }
+}
---
+++ crop-service/src/main/java/org/ead2/crop/CropApplication.java
@@ -0,0 +1,11 @@
+package org.ead2.crop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CropApplication.class, args);
+    }
+}
