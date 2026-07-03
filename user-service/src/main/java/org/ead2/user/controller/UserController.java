fce802e3dbad09f1cb22ef95d983db5458b36e0c
Dulan Dhanush <dulandhanush0@gmail.com>
2026-07-04 03:08:22 +0530

    Update UserController before merging main

--- user-service/src/main/java/org/ead2/user/controller/UserController.java
+++ user-service/src/main/java/org/ead2/user/controller/UserController.java
@@ -2,10 +2,7 @@ package org.ead2.user.controller;
 
 import org.ead2.user.data.User;
 import org.ead2.user.service.UserService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigInteger;
 import java.util.List;
