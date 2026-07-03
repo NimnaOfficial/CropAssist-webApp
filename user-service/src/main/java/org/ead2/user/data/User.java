ae7cbefe6263e39f7bd6b2c4ddfbf7029a84a761
Dulan Dhanush <dulandhanush0@gmail.com>
2026-07-04 02:23:55 +0530

    added the entity class needed syntax

--- user-service/src/main/java/org/ead2/user/data/User.java
+++ user-service/src/main/java/org/ead2/user/data/User.java
@@ -1,10 +1,14 @@
 package org.ead2.user.data;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+import java.math.BigInteger;
 
+@Entity
+@Table(name = "users")
 public class User {
 
-
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private BigInteger id;
 }
