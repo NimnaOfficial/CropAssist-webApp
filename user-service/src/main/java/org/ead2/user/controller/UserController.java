b5bdc2f382e49ada6b18a7c5bfc5f7e77377e5fa
SANDANIMNE K G L <CODSE251F-003@student.nibm.lk>
2026-07-04 00:10:19 +0530

    Potential fix for pull request finding

--- user-service/src/main/java/org/ead2/user/controller/UserController.java
+++ user-service/src/main/java/org/ead2/user/controller/UserController.java
@@ -2,23 +2,25 @@ package org.ead2.user.controller;
 
 import org.ead2.user.data.User;
 import org.ead2.user.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-@RequestMapping(path="/Api")
+@RequestMapping(path = "/Api")
 @RestController
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
-    @PostMapping(path= "/user")
-    public User createUser(@RequestBody User user){
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    //this is just
+
+    @PostMapping(path = "/user")
+    public User createUser(@RequestBody User user) {
         return userService.createUser(user);
     }
-//this is just
 }
