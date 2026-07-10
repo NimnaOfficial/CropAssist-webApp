d6edda4d5e39ba667727b8038e828bd347170922
Huwinfenando <jhnemfdo274@gmail.com>
2026-07-10 18:09:03 +0530

    start crop controller

--- crop-service/src/main/java/org/ead2/crop/controller/CropController.java
+++ crop-service/src/main/java/org/ead2/crop/controller/CropController.java
@@ -1,8 +1,25 @@
 package org.ead2.crop.controller;
 
+import org.ead2.crop.service.CropService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * CropController is intended to handle HTTP requests for the Crop Service.
  * Currently, it is a placeholder for future endpoint implementations (e.g., adding, updating, or deleting crops).
  */
+@CrossOrigin(origins = "*") // Allows the frontend to communicate with this controller from any origin (e.g. localhost:3000)
+@RequestMapping(path = "/Api") // Base URL path for all endpoints in this controller
+@RestController // Marks this class as a REST API controller that automatically serializes responses to JSON
+
+
 public class CropController {
+
+    private final CropService cropService;
+    public CropController(CropService cropService) {
+
+        this.cropService = cropService;
+    }
+
 }
